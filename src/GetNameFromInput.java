490ab5d4e002bee9b30e25a1b54f799eb55403d7
Anton Malyar <antn.mlr@gmail.com>
2022-06-08 16:59:03 +0300

    created servlet that output name from input

---
+++ src/GetNameFromInput.java
@@ -0,0 +1,19 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet("/getName")
+public class GetNameFromInput extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter printWriter = resp.getWriter();
+        String user = req.getParameter("user");
+        printWriter.println("<h2> Welcome "+user+"</h2>");
+        printWriter.close();
+    }
+}
